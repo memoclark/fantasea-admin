28336ba4169b133e77e60810c009758cba142add
RK Bernardino Macion <memoclarkson03@gmail.com>
2022-10-22 18:55:02 +0800

    commit

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
+import { fontFamily, fontSize, typography } from "@mui/system";
 
 // color design tokens
 export const tokens = (mode) => ({
@@ -77,7 +78,7 @@ export const tokens = (mode) => ({
                 100: "#040509",
                 200: "#080b12",
                 300: "#0c101b",
-                400: "#101624",
+                400: "#f2f0f0",
                 500: "#141b2d",
                 600: "#434957",
                 700: "#727681",
@@ -119,3 +120,75 @@ export const tokens = (mode) => ({
             },
         }),
 });
+
+// mui theme settings
+export const themeSettings = (mode) => {
+    const colors = tokens(mode);
+
+    return {
+        palette: {
+            mode: mode,
+            ...(mode === 'dark'
+            ? {
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: colors.primary[500],
+                }
+            } : {
+                primary: {
+                    main: colors.primary[100],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: "#fcfcfc",
+                },
+            }),
+        
+        },
+        typography:{
+            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        }
+    };
+};
